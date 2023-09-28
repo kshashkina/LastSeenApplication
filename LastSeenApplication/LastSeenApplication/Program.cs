1dcaef67cb24f6293b1a18f31f99ed9526a44acc
Katherine Shashkina <kshashkina@kse.org.ua>
2023-09-28 23:08:05 +0300

    added "HttpClient" to make HTTP GET requests to the remote server's API

--- LastSeenApplication/LastSeenApplication/Program.cs
+++ LastSeenApplication/LastSeenApplication/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿namespace LastSeenApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var offset = 0;
+            while (true)
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = client
+                        .GetAsync(new Uri($"https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}"))
+                        .Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Code for handling the successful HTTP response goes here.
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: " + response.StatusCode);
+                    }
+                }
+   
+            }
+        }
+    }
+}
