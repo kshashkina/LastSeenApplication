94281dba238140b0f3aadca5b4252c7c2657e04c
Katherine Shashkina <kshashkina@kse.org.ua>
2023-10-19 17:11:37 +0300

    changed the way of data collection and added api for the second feature

--- LastSeenApplication/FeaturesAPI/Controllers/Assignment4Api.cs
+++ LastSeenApplication/FeaturesAPI/Controllers/Assignment4Api.cs
@@ -2,13 +2,13 @@
 using Newtonsoft.Json;
 using OnlineApi;
 
-[Route("api/stats/user/total")]
+[Route("api/stats/user")]
 [ApiController]
 public class TotalTimeOnline : ControllerBase
 {
     private readonly string filePathFirst = @"..\wasOnlineTime\bin\Debug\net7.0\online.json";
 
-    [HttpGet("users")]
+    [HttpGet("total")]
     public IActionResult GetOnlineUsersData([FromQuery] string id)
     {
         try
@@ -36,6 +36,47 @@ public class TotalTimeOnline : ControllerBase
                 : (int?)null;
 
             return Ok(new { usersOnline = timeOnline });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal Server Error: {ex.Message}");
+        }
+    }
+}
+
+[Route("api/stats/user/average")]
+[ApiController]
+public class AverageTimeOnline : ControllerBase
+{
+    private readonly string filePathFirst = @"..\averageTime\bin\Debug\net7.0\online.json";
+
+    [HttpGet()]
+    public IActionResult GetOnlineUsersData([FromQuery] string id)
+    {
+        try
+        {
+
+            if (!System.IO.File.Exists(filePathFirst))
+            {
+                return NotFound("JSON file not found");
+            }
+
+            List<int> onlineUsersDataList = new List<int>();
+
+            foreach (string line in System.IO.File.ReadLines(filePathFirst))
+            {
+                var onlineUserData = JsonConvert.DeserializeObject<OnlineUsersData>(line);
+
+                if (onlineUserData.userId == id)
+                {
+                    onlineUsersDataList.Add(onlineUserData.wasTimeOnline);
+                }
+            }
+
+            var averageWeek = (int)Math.Round(onlineUsersDataList.Average());
+            var averageDay = (int)(averageWeek / 7);
+
+            return Ok(new { averageWeek, averageDay });
         }
         catch (Exception ex)
         {
--- LastSeenApplication/averageTime/Program.cs
+++ LastSeenApplication/averageTime/Program.cs
@@ -1,162 +1,127 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Net.Http;
-using Newtonsoft.Json;
 using System.Threading;
+using System.Globalization;
 
-class Program
+bool shouldRun = true;
+Dictionary<Tuple<string, string>, int> onlineUsersCount = new Dictionary<Tuple<string, string>, int>();
+
+while (shouldRun)
 {
-    static void Main()
+    int offset = 0;
+    while (true)
     {
-        bool shouldRun = true;
-        Dictionary<string, Dictionary<string, int>> day = new Dictionary<string, Dictionary<string, int>>();
-        Dictionary<string, Dictionary<int, int>> week = new Dictionary<string, Dictionary<int, int>>();
-
-        while (shouldRun)
+        var userData = FetchUserData(offset);
+        if (userData == null || userData.Length == 0)
         {
-            int offset = 0;
-            int onlineUsers = 0;
-            while (true)
+            string filePath = "online.json";
+            if (File.Exists(filePath))
             {
-                var userData = FetchUserData(offset);
-                if (userData == null || userData.Length == 0)
-                {
-                    string filePath = "online.json";
-                    if (File.Exists(filePath))
-                    {
-                        File.Delete(filePath);
-                    }
+                File.Delete(filePath);
+            }
 
-                    foreach (var user in day)
-                    {
-                        foreach (var dayInfo in user.Value)
-                        {
-                            foreach (var weekInfo in week[user.Key])
-                            {
-                                var onlineUser = new OnlineUsersData
-                                {
-                                    userId = user.Key,
-                                    wasTimeOnlineDay = dayInfo.Key,
-                                    wasTimeOnlineTime = dayInfo.Value,
-                                    wasTimeOnlineWeek = weekInfo.Key,
-                                    wasTimeOnlineWeekTime = weekInfo.Value
-                                };
-                                string json = JsonConvert.SerializeObject(onlineUser);
-                                if (File.Exists(filePath))
-                                {
-                                    string existingContent = File.ReadAllText(filePath);
-                                    existingContent += '\n' + json;
-                                    File.WriteAllText(filePath, existingContent);
-                                }
-                                else
-                                {
-                                    File.WriteAllText(filePath, json);
-                                }
-                            }
-                        }
-                    }
-                    Thread.Sleep(1000);
-                    break;
+            foreach (var user in onlineUsersCount)
+            {
+                var userId = user.Key.Item1;
+                var weekNumber = user.Key.Item2;
+                var onlineUser = new OnlineUsersData
+                {
+                    userId = userId,
+                    weekNumber = weekNumber,
+                    wasTimeOnline = user.Value,
+                };
+                string json = JsonConvert.SerializeObject(onlineUser);
+                if (File.Exists(filePath))
+                {
+                    string existingContent = File.ReadAllText(filePath);
+                    existingContent += '\n' + json;
+                    File.WriteAllText(filePath, existingContent);
+                }
+                else
+                {
+                    File.WriteAllText(filePath, json);
+                }
+            }
+            Thread.Sleep(1000);
+            break;
+        }
+
+        foreach (var user in userData)
+        {
+            if (user.lastSeenDate != null)
+            {
+                string userId = user.userId;
+                string weekNumber = GetWeekOfYear(user.lastSeenDate.Value).ToString();
+
+                var key = new Tuple<string, string>(userId, weekNumber);
+
+                if (!onlineUsersCount.ContainsKey(key))
+                {
+                    onlineUsersCount[key] = 0;
                 }
 
-                foreach (var user in userData)
-                {
-                    string todayString = DateTime.Now.Date.ToString("yyyy-MM-dd");
-                    int weekNumber = GetWeekNumber(DateTime.Now);
-                    if (user.lastSeenDate == null)
-                    {
-                        if (day.ContainsKey(user.userId))
-                        {
-                            if (day[user.userId].ContainsKey(todayString))
-                            {
-                                day[user.userId][todayString]++;
-                            }
-                            else
-                            {
-                                day[user.userId][todayString] = 1;
-                            }
-
-                            if (week.ContainsKey(user.userId))
-                            {
-                                if (week[user.userId].ContainsKey(weekNumber))
-                                {
-                                    week[user.userId][weekNumber]++;
-                                }
-                                else
-                                {
-                                    week[user.userId][weekNumber] = 1;
-                                }
-                            }
-                            else
-                            {
-                                week[user.userId] = new Dictionary<int, int> { { weekNumber, 1 } };
-                            }
-                        }
-                        else
-                        {
-                            day[user.userId] = new Dictionary<string, int> { { todayString, 1 } };
-                            week[user.userId] = new Dictionary<int, int> { { weekNumber, 1 } };
-                        }
-                    }
-                }
-
-                offset += userData.Length;
+                onlineUsersCount[key]++;
             }
         }
+
+        offset += userData.Length;
+    }
+}
+
+// Остальной код остается без изменений
+
+static int GetWeekOfYear(DateTime date)
+{
+    var ci = CultureInfo.CurrentCulture;
+    var cal = ci.Calendar;
+    var calWeekRule = ci.DateTimeFormat.CalendarWeekRule;
+    var calFirstDayOfWeek = ci.DateTimeFormat.FirstDayOfWeek;
+    return cal.GetWeekOfYear(date, calWeekRule, calFirstDayOfWeek);
+}
+
+static User[] FetchUserData(int offset, HttpClient client = null)
+{
+    if (client == null)
+    {
+        client = new HttpClient();
     }
 
-    static int GetWeekNumber(DateTime date)
+    using (client)
     {
-        CultureInfo ciCurr = CultureInfo.CurrentCulture;
-        int weekNum = ciCurr.Calendar.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
-        return weekNum;
-    }
+        HttpResponseMessage response = client
+            .GetAsync(new Uri($"https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}"))
+            .Result;
 
-    static User[] FetchUserData(int offset, HttpClient client = null)
-    {
-        if (client == null)
+        if (response.IsSuccessStatusCode)
         {
-            client = new HttpClient();
+            string jsonData = response.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<UserData>(jsonData)?.data;
         }
-
-        using (client)
+        else
         {
-            HttpResponseMessage response = client
-                .GetAsync(new Uri($"https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}"))
-                .Result;
-
-            if (response.IsSuccessStatusCode)
-            {
-                string jsonData = response.Content.ReadAsStringAsync().Result;
-                return JsonConvert.DeserializeObject<UserData>(jsonData)?.data;
-            }
-            else
-            {
-                Console.WriteLine("Error: " + response.StatusCode);
-                return null;
-            }
+            Console.WriteLine("Error: " + response.StatusCode);
+            return null;
         }
     }
+}
 
-    public class User
-    {
-        public DateTime? lastSeenDate { get; set; }
-        public string userId { get; set; }
-    }
+public class User
+{
+    public DateTime? lastSeenDate { get; set; }
+    public string userId { get; set; }
+}
 
-    public class UserData
-    {
-        public User[] data { get; set; }
-    }
+public class UserData
+{
+    public User[] data { get; set; }
+}
 
-    public class OnlineUsersData
-    {
-        public string userId { get; set; }
-        public string wasTimeOnlineDay { get; set; }
-        public int wasTimeOnlineTime { get; set; }
-        public int wasTimeOnlineWeek { get; set; }
-        public int wasTimeOnlineWeekTime { get; set; }
-    }
+public class OnlineUsersData
+{
+    public string userId { get; set; }
+    public string weekNumber { get; set; }
+    public int wasTimeOnline { get; set; }
 }
