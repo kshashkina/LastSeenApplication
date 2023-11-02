aefc68d727d418e49cad45b1029abe6c14b221c8
Katherine Shashkina <kshashkina@kse.org.ua>
2023-11-02 09:22:26 +0200

    e2e test

--- LastSeenApplication/E2ETest/E2ETest.cs
+++ LastSeenApplication/E2ETest/E2ETest.cs
@@ -17,8 +17,17 @@ public class E2ETests
         var localization = new Localization();
         string resultEN = localization.LanguageKey(1);
         var text = localization.Output(resultEN);
-        string expectedOutput = "What you want to do? \nHave a list of all users - 1 \nHave number of users at the exact time - 2\n Check if the user was online at the exact date - 3\nPrediction about amount of the users online - 4\nPrediction about user online - 5";
-        
+        string expectedOutput = "What you want to do?\n" +
+                                "Have a list of all users - 1\n" +
+                                "Have the number of users at the exact time - 2\n" +
+                                "Check if the user was online at the exact date - 3\n" +
+                                "Prediction about the number of users online - 4\n" +
+                                "Prediction about a user being online - 5\n" +
+                                "Total amount of time online for a user - 6\n" +
+                                "Average time for a user - 7\n" +
+                                "Display deleted user - 8\n" +
+                                "Post report - 9\n" + 
+                                "Get report - 10";
         var httpClient = new HttpClient();
         var result =  Program.FetchUserData(0, httpClient);
         var resultOnlineCount = await Program.GetOnlineUsersCount("2023-19-10-21:33:38");
@@ -30,6 +39,7 @@ public class E2ETests
         var resultTotalTime = await Program.GetTotalTimeForUser("938a6656-0b54-6a9c-76a2-bbfac8f3de81");
         var resultAverageTime = await Program.GetAverageTimeForUser("938a6656-0b54-6a9c-76a2-bbfac8f3de81");
         var resultDeleteUser = await Program.DeleteUser("b9cd6eac-1ff3-fa09-703e-e09241c4b33c");
+        var allUsers = await Program.GetAllUsers();
         
         Assert.Equal("en", resultEN);
         Assert.Equal(expectedOutput, text);
@@ -42,6 +52,9 @@ public class E2ETests
         Assert.Equal("{\"usersOnline\":42}",resultTotalTime);
         Assert.Equal("{\"averageWeek\":22,\"averageDay\":3}",resultAverageTime);
         Assert.Equal("{\"id\":\"b9cd6eac-1ff3-fa09-703e-e09241c4b33c\"}",resultDeleteUser);
+        Assert.NotNull(allUsers);
+        Assert.Contains("Doug93", allUsers);
+        Assert.Contains("a807e6f7-ec9c-f8a6-a6e4-43b8f36c78cc", allUsers);
 
     }
 }
