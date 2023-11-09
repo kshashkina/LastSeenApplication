c9f4e189fb9d0e70ab8565843ac0f71a85bb8aea
Katherine Shashkina <kshashkina@kse.org.ua>
2023-11-09 10:42:02 +0200

    removed code dublications and combined all data collection in one file

---
+++ LastSeenApplication/DataCollection/Program.cs
@@ -0,0 +1,221 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Globalization;
+
+public class User
+{
+    public DateTime? LastSeenDate { get; set; }
+    public string UserId { get; set; }
+    public string Nickname { get; set; }
+}
+
+public class OnlineUsersData
+{
+    public string UserId { get; set; }
+    public string LastSeen { get; set; }
+    public string Nickname { get; set; }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        // First set of code
+        bool shouldRun1 = true;
+        Dictionary<string, int> onlineUsersCount1 = new Dictionary<string, int>();
+
+        while (shouldRun1)
+        {
+            int offset = 0;
+            int onlineUsers = 0;
+            while (true)
+            {
+                var userData = FetchUserData(offset);
+                if (userData == null || userData.Length == 0)
+                {
+                    string filePath = "online.json";
+                    if (File.Exists(filePath))
+                    {
+                        File.Delete(filePath);
+                    }
+
+                    foreach (var user in onlineUsersCount1)
+                    {
+                        var onlineUser = new OnlineUsersData
+                        {
+                            UserId = user.Key,
+                            LastSeen = DateTime.Now.ToString(),
+                            Nickname = user.Value.ToString()
+                        };
+                        SaveOnlineUserData(onlineUser);
+                    }
+                    Thread.Sleep(1000);
+                    break;
+                }
+
+                foreach (var user in userData)
+                {
+                    if (user.LastSeenDate == null)
+                    {
+                        if (onlineUsersCount1.ContainsKey(user.UserId))
+                        {
+                            onlineUsersCount1[user.UserId]++;
+                        }
+                        else
+                        {
+                            onlineUsersCount1[user.UserId] = 1;
+                        }
+                    }
+                }
+
+                offset += userData.Length;
+            }
+        }
+
+        // Second set of code
+        bool shouldRun2 = true;
+        Dictionary<Tuple<string, string, string>, int> onlineUsersCount2 = new Dictionary<Tuple<string, string, string>, int>();
+
+        while (shouldRun2)
+        {
+            int offset = 0;
+            while (true)
+            {
+                var userData = FetchUserData(offset);
+                if (userData == null || userData.Length == 0)
+                {
+                    string filePath = "online.json";
+                    if (File.Exists(filePath))
+                    {
+                        File.Delete(filePath);
+                    }
+
+                    foreach (var user in onlineUsersCount2)
+                    {
+                        var userId = user.Key.Item1;
+                        var weekNumber = user.Key.Item2;
+                        var day = user.Key.Item3;
+                        var onlineUser = new OnlineUsersData
+                        {
+                            UserId = userId,
+                            LastSeen = DateTime.Now.ToString(),
+                            Nickname = user.Value.ToString()
+                        };
+                        SaveOnlineUserData(onlineUser);
+                    }
+                    Thread.Sleep(1000);
+                    break;
+                }
+
+                foreach (var user in userData)
+                {
+                    if (user.LastSeenDate != null)
+                    {
+                        string userId = user.UserId;
+                        string weekNumber = GetWeekOfYear(user.LastSeenDate.Value).ToString();
+                        string day = user.LastSeenDate.Value.ToString("yyyy-MM-dd");
+
+                        var key = new Tuple<string, string, string>(userId, weekNumber, day);
+                        if (!onlineUsersCount2.ContainsKey(key))
+                        {
+                            onlineUsersCount2[key] = 0;
+                        }
+
+                        onlineUsersCount2[key]++;
+                    }
+                }
+
+                offset += userData.Length;
+            }
+        }
+
+        // Third set of code
+        bool shouldRun3 = true;
+
+        while (shouldRun3)
+        {
+            int offset = 0;
+            while (true)
+            {
+                var userData = FetchUserData(offset);
+                if (userData == null || userData.Length == 0)
+                {
+                    return;
+                }
+                foreach (var user in userData)
+                {
+                    var onlineUser = new OnlineUsersData
+                    {
+                        UserId = user.UserId,
+                        LastSeen = user.LastSeenDate.HasValue ? user.LastSeenDate.ToString() : DateTime.Now.ToString(),
+                        Nickname = user.Nickname
+                    };
+                    SaveOnlineUserData(onlineUser);
+                }
+
+                offset += 20;
+            }
+            Thread.Sleep(10000);
+        }
+    }
+
+    static void SaveOnlineUserData(OnlineUsersData onlineUser)
+    {
+        string json = JsonConvert.SerializeObject(onlineUser);
+        string filePath = "online.json";
+        if (File.Exists(filePath))
+        {
+            string existingContent = File.ReadAllText(filePath);
+            existingContent += '\n' + json;
+            File.WriteAllText(filePath, existingContent);
+        }
+        else
+        {
+            File.WriteAllText(filePath, json);
+        }
+    }
+
+    static User[] FetchUserData(int offset, HttpClient client = null)
+    {
+        if (client == null)
+        {
+            client = new HttpClient();
+        }
+
+        using (client)
+        {
+            HttpResponseMessage response = client
+                .GetAsync(new Uri($"https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}"))
+                .Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonData = response.Content.ReadAsStringAsync().Result;
+                return JsonConvert.DeserializeObject<UserData>(jsonData)?.Data;
+            }
+            else
+            {
+                Console.WriteLine("Error: " + response.StatusCode);
+                return null;
+            }
+        }
+    }
+
+    static int GetWeekOfYear(DateTime date)
+    {
+        var ci = CultureInfo.CurrentCulture;
+        var cal = ci.Calendar;
+        var calWeekRule = ci.DateTimeFormat.CalendarWeekRule;
+        var calFirstDayOfWeek = ci.DateTimeFormat.FirstDayOfWeek;
+        return cal.GetWeekOfYear(date, calWeekRule, calFirstDayOfWeek);
+    }
+}
+
+public class UserData
+{
+    public User[] Data { get; set; }
+}
--- LastSeenApplication/LastSeenApplication/LastSeenApplication.cs
+++ LastSeenApplication/LastSeenApplication/LastSeenApplication.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace LastSeenApplication
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             Localization localization = new Localization();
             var language = localization.ChooseLanguage();
             localization.Output(language);
+
             int input = Convert.ToInt32(Console.ReadLine());
+
             switch (input)
             {
                 case 1:
@@ -18,125 +22,104 @@ namespace LastSeenApplication
                     break;
                 case 2:
                     var date = localization.FirstFeatureTranslation(language);
-                    GetOnlineUsersCount(date);
+                    await GetOnlineUsersCount(date);
                     break;
                 case 3:
-                    var dateUser = "";
-                    var id = "";
-                    (dateUser, id) = localization.SecondFeatureTranslation(language);
-                    GetUserDate(dateUser, id);
+                    var (dateUser, id) = localization.SecondFeatureTranslation(language);
+                    await GetUserDate(dateUser, id);
                     break;
                 case 4:
                     var datePrediction = localization.FirstFeatureTranslation(language);
-                    GetPredictionOnline(datePrediction);
+                    await GetPredictionOnline(datePrediction);
                     break;
                 case 5:
-                    var datePredictionUser = "";
-                    var idPredictionUser = "";
-                    var tolerance = "";
-                    (datePrediction, idPredictionUser, tolerance) = localization.ForthFeatureTranslation(language);
-                    GetPredictionOnlineUser(datePredictionUser, tolerance, idPredictionUser);
+                    var (datePredictionUser, idPredictionUser, tolerance) = localization.ForthFeatureTranslation(language);
+                    await GetPredictionOnlineUser(datePredictionUser, tolerance, idPredictionUser);
                     break;
                 case 6:
                     var idTotal = localization.Assignment4Translation(language);
-                    GetTotalTimeForUser(idTotal);
+                    await GetTotalTimeForUser(idTotal);
                     break;
                 case 7:
                     var idAverage = localization.Assignment4Translation(language);
-                    GetAverageTimeForUser(idAverage);
+                    await GetAverageTimeForUser(idAverage);
                     break;
                 case 8:
                     var idDelete = localization.Assignment4Translation(language);
-                    DeleteUser(idDelete);
+                    await DeleteUser(idDelete);
                     break;
                 case 9:
-                    var reportNamePost = Console.ReadLine();
-                    var users = Console.ReadLine();
-                    var metrics = Console.ReadLine();
-                    PostReport(reportNamePost, users, metrics);
+                    var (reportNamePost, users, metrics) = localization.FifthFeatureTranslationPost(language);
+                    await PostReport(reportNamePost, users, metrics);
                     break;
                 case 10:
-                    var reportNameGet = Console.ReadLine();
-                    var from = Console.ReadLine();
-                    var to = Console.ReadLine();
-                    GetReport(reportNameGet, from, to);
+                    var (reportNameGet, from, to) = localization.FifthFeatureTranslationGet(language);
+                    await GetReport(reportNameGet, from, to);
                     break;
-
+                case 11:
+                    await GetAllUsers();
+                    break;
             }
         }
-        
+
         public static async Task<string> GetOnlineUsersCount(string date)
         {
             string apiUrl = $"http://localhost:5169/api/stats/users?date={date}";
-
-            Loader loader = new Loader();
-            var result = await loader.Load(apiUrl);
-            return result;
+            return await LoadData(apiUrl);
         }
-        
+
         public static async Task<string> GetUserDate(string date, string id)
         {
             string apiUrl = $"http://localhost:5169/api/stats/user?date={date}&userId={id}";
-            Loader loader = new Loader();
-            var result = await loader.Load(apiUrl);
-            return result;
+            return await LoadData(apiUrl);
         }
-        
+
         public static async Task<string> GetPredictionOnline(string date)
         {
             string apiUrl = $"http://localhost:5169/api/prediction/user/average?date={date}";
-
-            Loader loader = new Loader();
-            var result = await loader.Load(apiUrl);
-            return result;
+            return await LoadData(apiUrl);
         }
-        
+
         public static async Task<string> GetPredictionOnlineUser(string date, string tolerance, string id)
         {
             string apiUrl = $"http://localhost:5169/api/prediction/user/status?date={date}&tolerance={tolerance}&userId={id}";
-
-            Loader loader = new Loader();
-            var result = await loader.Load(apiUrl);
-            return result;
+            return await LoadData(apiUrl);
         }
 
         public static async Task<string> GetTotalTimeForUser(string id)
         {
             string apiUrl = $"http://localhost:5169/api/stats/user/total?id={id}";
-            Loader loader = new Loader();
-            var result = await loader.Load(apiUrl);
-            return result;
+            return await LoadData(apiUrl);
         }
 
         public static async Task<string> GetAverageTimeForUser(string id)
         {
             string apiUrl = $"http://localhost:5169/api/stats/user/average?id={id}";
-            Loader loader = new Loader();
-            var result = await loader.Load(apiUrl);
-            return result;
+            return await LoadData(apiUrl);
         }
-
+        
         public static async Task<string> DeleteUser(string id)
         {
             string apiUrl = $"http://localhost:5169/api/user/forget?id={id}";
-            Loader loader = new Loader();
-            var result = await loader.Post(apiUrl, id);
-            return result;
+            return await PostData(apiUrl, id);
         }
 
         public static async Task<string> PostReport(string reportName, string users, string metrics)
         {
             string apiUrl = $"http://localhost:5169/api/report/{reportName}?users={users}&metrics={metrics}";
-            Loader loader = new Loader();
-            var result = await loader.Post(apiUrl, "{}");
-            return result;
+            return await PostData(apiUrl, "{}");
         }
+
         public static async Task<string> GetReport(string reportName, string from, string to)
         {
             string apiUrl = $"http://localhost:5169/api/report/{reportName}?from={from}&to={to}";
-            Loader loader = new Loader();
-            var result = await loader.Load(apiUrl);
-            return result;
+            return await LoadData(apiUrl);
+        }
+
+        public static async Task<string> GetAllUsers()
+        {
+            var apiUrl = "http://localhost:5169/api/users/list";
+            return await LoadData(apiUrl);
         }
 
         public static void RunLastSeenApplication(int startingOffset, string language)
@@ -158,9 +141,10 @@ namespace LastSeenApplication
                     Console.WriteLine(formattedData);
                 }
 
-                offset += userData.Length; 
+                offset += userData.Length;
             }
         }
+
         public static User[] FetchUserData(int offset, HttpClient client = null)
         {
             if (client == null)
@@ -185,6 +169,20 @@ namespace LastSeenApplication
                     return null;
                 }
             }
+        }
+
+        public static async Task<string> LoadData(string apiUrl)
+        {
+            Loader loader = new Loader();
+            var result = await loader.Load(apiUrl);
+            return result;
+        }
+
+        public static async Task<string> PostData(string apiUrl, string data)
+        {
+            Loader loader = new Loader();
+            var result = await loader.Post(apiUrl, data);
+            return result;
         }
     }
 
