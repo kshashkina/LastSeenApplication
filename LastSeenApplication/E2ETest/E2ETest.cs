de32d5533ca7e67c4b7e46f9f017bb6616c69fd6
Katherine Shashkina <kshashkina@kse.org.ua>
2023-11-09 12:25:45 +0200

    e2e tests

--- LastSeenApplication/E2ETest/E2ETest.cs
+++ LastSeenApplication/E2ETest/E2ETest.cs
@@ -31,6 +31,9 @@ public class E2ETests
         var resultAverageTime = await Program.GetAverageTimeForUser("938a6656-0b54-6a9c-76a2-bbfac8f3de81");
         var resultDeleteUser = await Program.DeleteUser("b9cd6eac-1ff3-fa09-703e-e09241c4b33c");
         var resultReport = await Program.GetReport("report", "2023-10-30", "2023-10-31");
+        var resultAllReports = await Program.GetAllReports();
+        string jsonData = "[{\"name\":\"report-2023-10-30.json\",\"_report\":\"{\\\"Metrics\\\":{\\\"DailyAverage\\\":8,\\\"WeeklyAverage\\\":56,\\\"Total\\\":35,\\\"Min\\\":6,\\\"Max\\\":35},\\\"Users\\\":\\\"1fa8819c-1386-dc9b-840b-e7d0dc84f158\\\"}\"},{\"name\":\"report-2023-10-31.json\",\"_report\":\"{\\\"Metrics\\\":{\\\"DailyAverage\\\":8,\\\"WeeklyAverage\\\":56,\\\"Total\\\":35,\\\"Min\\\":35,\\\"Max\\\":35},\\\"Users\\\":\\\"1fa8819c-1386-dc9b-840b-e7d0dc84f158\\\"}\"},{\"name\":\"report-2023-11-01.json\",\"_report\":\"{\\\"Metrics\\\":{\\\"DailyAverage\\\":null,\\\"WeeklyAverage\\\":null,\\\"Total\\\":35,\\\"Min\\\":null,\\\"Max\\\":null},\\\"Users\\\":\\\"1fa8819c-1386-dc9b-840b-e7d0dc84f158\\\"}\"}]";
+
         
         Assert.Equal("en", resultEN);
         Assert.Equal(expectedOutput, text);
@@ -44,6 +47,8 @@ public class E2ETests
         Assert.Equal("{\"averageWeek\":22,\"averageDay\":3}",resultAverageTime);
         Assert.Equal("{\"id\":\"b9cd6eac-1ff3-fa09-703e-e09241c4b33c\"}",resultDeleteUser);
         Assert.Equal(@"{""report"":{""5ed89705-d3c6-6dea-880c-82ad5c185285"":{""dailyAverage"":7,""weeklyAverage"":52,""total"":10,""min"":5,""max"":5},""1fa8819c-1386-dc9b-840b-e7d0dc84f158"":{""dailyAverage"":8,""weeklyAverage"":56,""total"":70,""min"":35,""max"":35}}}", resultReport);
+        Assert.NotNull(result);
+        Assert.Equal(jsonData, resultAllReports);
 
     }
 }
